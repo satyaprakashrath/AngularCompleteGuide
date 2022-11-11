780fd9743d472ba3d18e13115b64e12377e06a3e
satya <satyaprakash.rath@clarivate.com>
2022-11-11 18:08:47 +0530

    Initail Recipe item

--- ng-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes : Recipe[] = [new Recipe("Chicken Rice", "Butter Chicken and Jira Rice", "https://img.buzzfeed.com/buzzfeed-static/static/2022-03/5/0/asset/6201713e5c7e/sub-buzz-1009-1646440684-8.jpg")];
 
   constructor() { }
 
